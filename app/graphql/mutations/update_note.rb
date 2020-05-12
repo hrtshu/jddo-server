75f17fcff142d8b95d795fc9cb47cbe724f017b7
Shuhei <sh7916@gmail.com>
2020-05-12 12:17:17 +0900

    Implement UpdateNote mutation
    Reference: https://qiita.com/k-penguin-sato/items/07fef2f26fd6339e0e69#mutation-update

--- app/graphql/mutations/update_note.rb
+++ app/graphql/mutations/update_note.rb
@@ -1,13 +1,17 @@
 module Mutations
-  class CreateNote < BaseMutation
+  class UpdateNote < BaseMutation
     field :note, Types::NoteType, null: true
     field :result, Boolean, null: true
 
     argument :note, Types::NoteInputType, required: true
 
     def resolve(**args)
-      # TODO args[:note].idが存在する場合にエラーを吐く？
-      note = Note.create(subject: args[:note].subject, body: args[:note].body)
+      note = Note.find(args[:note].id)
+
+      mutation_arg = args[:note].to_kwargs
+      mutation_arg.delete(:id)
+      note.update(mutation_arg)
+
       {
         note: note,
         result: note.errors.blank? # TODO その他エラーも適切にハンドル
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_note, mutation: Mutations::CreateNote
+    field :update_note, mutation: Mutations::UpdateNote
   end
 end
